06ae9b4842d3637e080dfad11a37def3e4269642
jackhou2007 <jackhou2007@gmail.com>
2015-02-06 23:57:21 +0800

    add file

---
+++ control-flow/binsearch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int binsearch(int val, int v[], int n);
+
+
+int main(void) {
+    return 0;
+}
+
+int binsearch(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid]) {
+            high = mid - 1;
+        } else if (x > v[mid]) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
